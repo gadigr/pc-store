95446ebd8866961a8d0b019a4aca5bd5724fabd4
Gadi Gomez <gadigr@gmail.com>
2015-12-29 00:04:32 +0200

    added canvas logo

--- client/controllers/homeCtrl.js
+++ client/controllers/homeCtrl.js
@@ -2,6 +2,17 @@ app.controller('homeCtrl', function($scope, $http) {
 
     console.log('home');
 
+    var c = document.getElementById("myCanvas");
+    var ctx = c.getContext("2d");
+    ctx.font = "30px Arial";
+    var gradient=ctx.createLinearGradient(0,0,c.width,0);
+    gradient.addColorStop("0",'white');
+    gradient.addColorStop("0.5","blue");
+    gradient.addColorStop("1.0","white");
+// Fill with gradient
+    ctx.fillStyle=gradient;
+    ctx.fillText("Laptops'R'Us!",9,35);
+
     $http.get('/home/getLaptops')
         .then(function (response){
            console.log(response.data);
