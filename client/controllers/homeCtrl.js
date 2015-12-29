d856939d4ff0debf2908f9eda500138a813f9a69
Gadi Gomez <gadigr@gmail.com>
2015-12-29 18:56:21 +0200

    added comments

--- client/controllers/homeCtrl.js
+++ client/controllers/homeCtrl.js
@@ -1,13 +1,17 @@
 app.controller('homeCtrl', function($scope, $http) {
 
+    // build canvas
     var c = document.getElementById("myCanvas");
     var ctx = c.getContext("2d");
     ctx.font = "30px Arial";
+
+    // build gradient
     var gradient=ctx.createLinearGradient(0,0,c.width,0);
     gradient.addColorStop("0",'white');
     gradient.addColorStop("0.5","blue");
     gradient.addColorStop("1.0","white");
-// Fill with gradient
+
+    // Fill with gradient
     ctx.fillStyle=gradient;
     ctx.fillText("Laptops'R'Us!",9,35);
 
